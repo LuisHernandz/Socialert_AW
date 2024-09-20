06e6d01c6ebe7764f9af0a82deab2447082b8a92
UserEdd <eduardodominguez@laselva.edu.mx>
2024-09-20 13:03:39 -0600

    Actualizacion nombre

--- app/Http/Controllers/BackupController.php
+++ app/Http/Controllers/BackupController.php
@@ -14,16 +14,12 @@ class BackupController extends Controller
         $fecha = date('d-m-Y');
         $nombreArchivo = 'Backup-SOCIALERT' . $fecha . '.zip';
         
-        // Llamada al comando Artisan de forma interna, sin necesidad de exec()
         Artisan::call('backup:run', ['--only-db' => true, '--filename' => $nombreArchivo]);
 
-        // Registrar salida en los logs
         Log::info('Backup ejecutado con nombre de archivo: ' . $nombreArchivo);
 
-        // Verificar la ruta del archivo
         $rutaArchivo = storage_path('app/Laravel/' . $nombreArchivo);
 
-        // Verificar si el archivo existe
         if (Storage::exists('Laravel/' . $nombreArchivo)) {
             Log::info('Archivo encontrado: ' . $rutaArchivo);
             return response()->download($rutaArchivo)->deleteFileAfterSend();
